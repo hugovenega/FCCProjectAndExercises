d6739d93e5681ed6ec7a418a91c7e4ca74485d6d
hugo venega <hugo.venega.00@gmail.com>
2021-08-26 14:48:35 -0300

    Add cesarEncryption project

---
+++ fcc_project/cesarEncryption.js
@@ -0,0 +1,21 @@
+
+function rot13(str) {
+const strPLetter = str.split("");
+const arrayCodes = [];
+const  arrayNewLetters= [];
+for (let i = 0; i < strPLetter.length; i += 1){
+  if ((strPLetter[i].charCodeAt(0) < 65) || (strPLetter[i].charCodeAt(0) > 90)){
+    arrayCodes.push(strPLetter[i].charCodeAt(0))
+  } else if ((strPLetter[i].charCodeAt(0) - 13) < 65){
+    let rest = (65 - (strPLetter[i].charCodeAt(0)-12)); 
+    arrayCodes.push(90 - rest);
+  } else {
+    arrayCodes.push(strPLetter[i].charCodeAt(0) -13);
+  }
+}
+for (let i = 0; i < arrayCodes.length; i += 1){
+  arrayNewLetters.push(String.fromCharCode(arrayCodes[i]));
+}
+return (arrayNewLetters.join(""));
+}
+console.log(rot13("SERR PBQR PNZC!"));

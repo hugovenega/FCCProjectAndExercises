7a700f131438dbab7d9825943ebb3d06b66c2f4a
hugo venega <hugo.venega.00@gmail.com>
2021-08-30 17:20:01 -0300

    factor the cesarEncryption function by replacing for loop with map

--- fcc_project/cesarEncryption.js
+++ fcc_project/cesarEncryption.js
@@ -1,20 +1,19 @@
 function rot13(str) {
   const strPLetter = str.split('');
-  const arrayCodes = [];
-  const arrayNewLetters = [];
-  for (let i = 0; i < strPLetter.length; i += 1) {
-    if ((strPLetter[i].charCodeAt(0) < 65) || (strPLetter[i].charCodeAt(0) > 90)) {
-      arrayCodes.push(strPLetter[i].charCodeAt(0));
-    } else if ((strPLetter[i].charCodeAt(0) - 13) < 65) {
-      const rest = (65 - (strPLetter[i].charCodeAt(0) - 12));
-      arrayCodes.push(90 - rest);
-    } else {
-      arrayCodes.push(strPLetter[i].charCodeAt(0) - 13);
+  const getCodeLetters = function(letter) {
+    if ((letter.charCodeAt(0) < 65) || (letter.charCodeAt(0) > 90)) {
+      return (letter.charCodeAt(0));
+    } if ((letter.charCodeAt(0) - 13) < 65) {
+      const rest = (65 - (letter.charCodeAt(0) - 12));
+      return (90 - rest);
     }
-  }
-  for (let i = 0; i < arrayCodes.length; i += 1) {
-    arrayNewLetters.push(String.fromCharCode(arrayCodes[i]));
-  }
-  return (arrayNewLetters.join(''));
+    return (letter.charCodeAt(0) - 13);
+  };
+  const getNewLetters = function(code) {
+    return String.fromCharCode(code);
+  };
+  const arrayCodes = strPLetter.map((letter) => getCodeLetters(letter));
+  const arrayNewLetters = arrayCodes.map((code) => getNewLetters(code));
+  return arrayNewLetters.join('');
 }
 module.exports = rot13;

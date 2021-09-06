dcacb83edb1a467de7755cab4dd16daef3914d20
hugo venega <hugo.venega.00@gmail.com>
2021-09-06 19:07:40 -0300

    checkCashRegister function update

--- fcc_project/box.js
+++ fcc_project/box.js
@@ -1,63 +1,49 @@
 const conversionRate = {
   'ONE HUNDRED': 100,
-  'TWENTY': 20,
-  'TEN': 10,
-  'FIVE': 5,
-  'ONE': 1,
-  'QUARTER': 0.25,
-  'DIME': 0.1,
-  'NICKEL': 0.5,
-  'PENNY': 0.01
+  TWENTY: 20,
+  TEN: 10,
+  FIVE: 5,
+  ONE: 1,
+  QUARTER: 0.25,
+  DIME: 0.1,
+  NICKEL: 0.05,
+  PENNY: 0.01,
 };
 
-
 function checkCashRegister(price, cash, cid) {
-  const [penny, nickel, dime, quarter, one, five, ten, twenty, oneHundred] = cid;
-  const restTotal = cash - price;
-  let rest = restTotal;
-  let state;
-  const cashInCid = getTotalCash(cid);
-  let array = [];
-  if (cashInCid >= rest) {
-    state = 'OPEN';
-    rest = cid.reduce((rest, [bill, value]) => {
-      return getQuantity(value, bill, conversionRate[bill], rest );
-    }, rest)
-    if ((rest === 0.01) && (penny[1] > array[array.length - 1][1])) {
-      array[array.length - 1][1] = array[array.length - 1][1] + 0.01;
-      rest -= 0.01;
+  let changeSum = parseFloat((cash - price).toFixed(2));
+  const changeSumCheck = changeSum;
+  let change = [];
+  let status = '';
+
+  let totalInCid = 0;
+  const filteredCid = cid.filter((elem) => elem[1] !== 0).reverse();
+
+  filteredCid.forEach((element) => {
+    const coin = element[0];
+    let coinSum = element[1];
+    totalInCid += coinSum;
+    let amount = 0;
+    while (changeSum >= conversionRate[coin] && coinSum > 0) {
+      amount += conversionRate[coin];
+      changeSum -= conversionRate[coin];
+      coinSum -= conversionRate[coin];
     }
+    if (amount !== 0) {
+      change.push([coin, amount]);
+    }
+  });
+  if (changeSum > 0) {
+    status = 'INSUFFICIENT_FOUNDS';
+    change = [];
+  } else if (changeSum === 0 && changeSumCheck === totalInCid) {
+    status = 'CLOSED';
+    change = cid;
+  } else {
+    status = 'OPEN';
   }
-  if (cashInCid === restTotal) {
-    state = 'CLOSED';
-    array = cid;
-  } else if (rest > 0) {
-    state = 'INSUFFICIENT_FUNDS';
-    array = [];
-  }
-  return { status: state, change: array };
+  return { 'status': status, 'change': change };
 }
 
-
-function getQuantity(inBox, bill, value, resto) {
-  const array = [];
-  let box = inBox;
-  let theRest = resto;
-  let countBill = 0;
-  while (box >= value && theRest >= value) {
-    box -= value;
-    theRest -= value;
-    countBill += 1;
-  }
-  const money = countBill * value;
-  if (countBill > 0) {
-    array.push([bill, money]);
-  }
-  return parseFloat((theRest).toFixed(2));
-}
-
-function getTotalCash(cid) {
-  return cid.reduce((acc, item) => acc + item[1], 0);
-}
-
+module.exports = checkCashRegister;
 module.exports = checkCashRegister;
--- fcc_project/box.spec.js
+++ fcc_project/box.spec.js
@@ -15,7 +15,7 @@ in addition to returning an array with the change to return specifying how much 
   });
   it('to pay 19.5 , pay with 20, there is no return due to lack of funds', () => {
     expect(checkCashRegister(19.5, 20, [['PENNY', 0.01], ['NICKEL', 0], ['DIME', 0], ['QUARTER', 0], ['ONE', 0], ['FIVE', 0], ['TEN', 0], ['TWENTY', 0], ['ONE HUNDRED', 0]]))
-      .toEqual({ status: 'INSUFFICIENT_FUNDS', change: [] });
+      .toEqual({ status: 'INSUFFICIENT_FOUNDS', change: [] });
   });
   it('to pay 19.5 , pay with 20, returned 0.5 with penny and close the cashregister', () => {
     expect(checkCashRegister(19.5, 20, [['PENNY', 0.5], ['NICKEL', 0], ['DIME', 0], ['QUARTER', 0], ['ONE', 0], ['FIVE', 0], ['TEN', 0], ['TWENTY', 0], ['ONE HUNDRED', 0]]))
@@ -23,6 +23,6 @@ in addition to returning an array with the change to return specifying how much 
   });
   it('to pay 19.5 , pay with 20, there is no return because there are no funds to give the exact change', () => {
     expect(checkCashRegister(19.5, 20, [['PENNY', 0.01], ['NICKEL', 0], ['DIME', 0], ['QUARTER', 0], ['ONE', 1], ['FIVE', 0], ['TEN', 0], ['TWENTY', 0], ['ONE HUNDRED', 0]]))
-      .toEqual({ status: 'INSUFFICIENT_FUNDS', change: [] });
+      .toEqual({ status: 'INSUFFICIENT_FOUNDS', change: [] });
   });
 });

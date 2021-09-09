b6dd1eec42a6e3f17014f0df704b583594db0d3c
Brian Alonso <brian.alonso.a@gmail.com>
2021-09-08 21:03:44 -0300

    Improve phoneChecker

--- fcc_project/phonechecker.js
+++ fcc_project/phonechecker.js
@@ -1,14 +1,13 @@
-function telephoneCheck(str) {
-  const regExps = [
-    /^((1\s\d{3}|\d{3}))\-\d{3}\-\d{4}/, // 1 555-555-5555 or 555-555-5555
-    /^(1\s|())\(\d{3}\)\d{3}\-\d{4}/, // 1 (555)555-5555 or (555)555-5555
-    /^(1\s|())\(\d{3}\)\s\d{3}\-\d{4}/, // 1 (555) 555-5555 or (555) 555-5555
-    /^(1\s\d{3}|\d{3})\s\d{3}\s\d{4}/, // 1 555 555 5555 or 555 555 5555
-    /^(1\s\d{10})|(^\d{10}$)/, // 1 5555555555 or 5555555555
-  ];
-  const result = regExps.some((regExp) => regExp.test(str));
+const validPhoneNumberRegex = [
+  /^((1\s\d{3}|\d{3}))\-\d{3}\-\d{4}/, // 1 555-555-5555 or 555-555-5555
+  /^(1\s|())\(\d{3}\)\d{3}\-\d{4}/, // 1 (555)555-5555 or (555)555-5555
+  /^(1\s|())\(\d{3}\)\s\d{3}\-\d{4}/, // 1 (555) 555-5555 or (555) 555-5555
+  /^(1\s\d{3}|\d{3})\s\d{3}\s\d{4}/, // 1 555 555 5555 or 555 555 5555
+  /^(1\s\d{10})|(^\d{10}$)/, // 1 5555555555 or 5555555555
+];
 
-  return result;
+function telephoneCheck(str) {
+  return validPhoneNumberRegex.some((regExp) => regExp.test(str));
 }
 
 module.exports = telephoneCheck;

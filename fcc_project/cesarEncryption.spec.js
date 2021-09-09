f21bc60de65f2c4a900de6ce66b982181ad61fb3
Brian Alonso <brian.alonso.a@gmail.com>
2021-09-08 21:03:13 -0300

    Improve cesarEntryption tests

--- fcc_project/cesarEncryption.spec.js
+++ fcc_project/cesarEncryption.spec.js
@@ -1,13 +1,15 @@
 const rot13 = require('./cesarEncryption');
 
-describe('testing encrypted words', () => {
-  it('GUR DHVPX OEBJA SBK WHZCF BIRE GUR YNML QBT.= "THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG."', () => {
-    expect(rot13('GUR DHVPX OEBJA SBK WHZCF BIRE GUR YNML QBT.')).toBe('THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG.');
-  });
-  it('SERR PBQR PNZC= "FREE CODE CAMP"', () => {
-    expect(rot13('SERR PBQR PNZC')).toBe('FREE CODE CAMP');
-  });
-  it('SERR CVMMN!= "FREE PIZZA!"', () => {
-    expect(rot13('SERR CVMMN!')).toBe('FREE PIZZA!');
+const expectations = [
+  ['GUR DHVPX OEBJA SBK WHZCF BIRE GUR YNML QBT.', 'THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG.'],
+  ['SERR PBQR PNZC', 'FREE CODE CAMP'],
+  ['SERR CVMMN!', 'FREE PIZZA!'],
+];
+
+describe('cesarEntryption', () => {
+  it('should rotate a set of expectations', () => {
+    expectations.forEach(([input, expected]) => {
+      expect(rot13(input)).toBe(expected);
+    });
   });
 });

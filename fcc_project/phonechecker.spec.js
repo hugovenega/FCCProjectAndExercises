6b6c745ce39f066f96b2d1df26a6f937bd6a9d0c
hugo venega <hugo.venega.00@gmail.com>
2021-09-15 16:15:27 -0300

    Improve tests of phonechecker

--- fcc_project/phonechecker.spec.js
+++ fcc_project/phonechecker.spec.js
@@ -1,90 +1,48 @@
 const telephoneCheck = require('./phonechecker');
 
-describe('telephonechecker test with standard pattern using USA country code', () => {
-  it('1 555-555-5555', () => {
-    expect(telephoneCheck('1 555-555-5555')).toBe(true);
+describe('telephoneChecker', () => {
+  describe('for all US phone spell patterns using your country code', () => {
+    const patternsWithUSACode = ['1 555-555-5555', '1 (555)555-5555', '1 (555) 555-5555', '1 555 555 5555', '1 5555555555'];
+    it('should return true', () => {
+      patternsWithUSACode.forEach((pattern) => {
+        expect(telephoneCheck(pattern)).toBe(true);
+      });
+    });
   });
-  it('1 (555)555-5555', () => {
-    expect(telephoneCheck('1 (555)555-5555')).toBe(true);
-  });
-  it('1 (555) 555-5555', () => {
-    expect(telephoneCheck('1 (555) 555-5555')).toBe(true);
-  });
-  it('1 555 555 5555', () => {
-    expect(telephoneCheck('1 555 555 5555')).toBe(true);
-  });
-  it('1 5555555555', () => {
-    expect(telephoneCheck('1 5555555555')).toBe(true);
-  });
-});
-describe('telephonechecker test with standard pattern without USA country code', () => {
-  it('555-555-5555', () => {
-    expect(telephoneCheck('555-555-5555')).toBe(true);
-  });
-  it('(555)555-5555', () => {
-    expect(telephoneCheck('(555)555-5555')).toBe(true);
-  });
-  it('(555) 555-5555', () => {
-    expect(telephoneCheck('(555) 555-5555')).toBe(true);
-  });
-  it('555 555 5555', () => {
-    expect(telephoneCheck('555 555 5555')).toBe(true);
-  });
-  it('5555555555', () => {
-    expect(telephoneCheck('5555555555')).toBe(true);
-  });
-});
 
-describe('telephonechecker test with numbers that respect the pattern using USA country code', () => {
-  it('1 234-567-8901', () => {
-    expect(telephoneCheck('1 234-567-8901')).toBe(true);
+  describe('for all US phone spell patterns', () => {
+    const patternsWithOutUSACode = ['555-555-5555', '(555)555-5555', '(555) 555-5555', '555 555 5555', '5555555555'];
+    it('should return true', () => {
+      patternsWithOutUSACode.forEach((pattern) => {
+        expect(telephoneCheck(pattern)).toBe(true);
+      });
+    });
   });
-  it('1 (232)564-9987', () => {
-    expect(telephoneCheck('1 (232)564-9987')).toBe(true);
-  });
-  it('1 (396) 500-2934', () => {
-    expect(telephoneCheck('1 (396) 500-2934')).toBe(true);
-  });
-  it('1 333 209 1029', () => {
-    expect(telephoneCheck('1 333 209 1029')).toBe(true);
-  });
-  it('1 3245610021', () => {
-    expect(telephoneCheck('1 3245610021')).toBe(true);
-  });
-});
 
-describe('telephonechecker test with numbers that respect the pattern without USA country code', () => {
-  it('234-675-1768', () => {
-    expect(telephoneCheck('234-675-1768')).toBe(true);
+  describe('for US phone spell using your country code', () => {
+    const phoneNumbersWithUSACode = ['1 234-567-8901', '1 (232)564-9987', '1 (396) 500-2934', '1 333 209 1029', '1 3245610021'];
+    phoneNumbersWithUSACode.forEach((pattern) => {
+      it(`should return true for ${pattern}`, () => {
+        expect(telephoneCheck(pattern)).toBe(true);
+      });
+    });
   });
-  it('(451)436-6711', () => {
-    expect(telephoneCheck('(451)436-6711')).toBe(true);
-  });
-  it('(209) 443-5510', () => {
-    expect(telephoneCheck('(209) 443-5510')).toBe(true);
-  });
-  it('532 888 1923', () => {
-    expect(telephoneCheck('532 888 1923')).toBe(true);
-  });
-  it('1120985834', () => {
-    expect(telephoneCheck('1120985834')).toBe(true);
-  });
-});
 
-describe('telephonechecker test with with wrong numbers, errors and different country code, this should return false', () => {
-  it('2 sixthree4 54% 324', () => {
-    expect(telephoneCheck('2 sixthree4 54% 324')).toBe(false);
+  describe('for US phone spell', () => {
+    const phoneNumbersWithOutUSACode = ['234-567-8901', '(232)564-9987', '(396) 500-2934', '333 209 1029', '3245610021'];
+    phoneNumbersWithOutUSACode.forEach((pattern) => {
+      it(`should return true for ${pattern}`, () => {
+        expect(telephoneCheck(pattern)).toBe(true);
+      });
+    });
   });
-  it('1 (432( 3455-9812', () => {
-    expect(telephoneCheck('1 (432( 3455-9812')).toBe(false);
-  });
-  it('1 (3233)342 444', () => {
-    expect(telephoneCheck('1 (3233) 342 444')).toBe(false);
-  });
-  it('532+888 1923', () => {
-    expect(telephoneCheck('532+888 1923')).toBe(false);
-  });
-  it('1120985834122', () => {
-    expect(telephoneCheck('1120985834122')).toBe(false);
+
+  describe('for any income that is not a US phone number', () => {
+    const notPhoneNumbers = ['2 sixthree4 54% 324', '1 (432( 3455-9812', '1 (3233)342 444', '532+888 1923', '1120985834122'];
+    notPhoneNumbers.forEach((pattern) => {
+      it(`should return false for ${pattern}`, () => {
+        expect(telephoneCheck(pattern)).toBe(false);
+      });
+    });
   });
 });
